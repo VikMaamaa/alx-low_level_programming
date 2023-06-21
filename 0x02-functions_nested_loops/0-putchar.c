b1d57f5c8e3fda6766b4f683412943438c1695dc
vikmaamaa <maamaavictor@yahoo.com>
2023-06-21 05:40:18 +0100

    done

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*main - prints _putchar, followed by a new line
+*Return:return 0
+*/
+int main(void)
+{
+	char school[10] = "_putchar";
+
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(school[i]);
+	}
+	_putchar(10);
+	return (0);
+}
