ca671d787454a44cd584581f9384829fc0eefddc
vikmaamaa <maamaavictor@yahoo.com>
2023-07-08 12:43:32 +0100

    done

---
+++ 0x09-static_libraries/functions/0-isupper.c
@@ -0,0 +1,14 @@
+#include "../main.h"
+
+/**
+ * _isupper - Not implemented
+ *
+ * @c: this is the entry
+ *
+ * Return: 0
+ *
+ **/
+int _isupper(int c)
+{
+	return (0);
+}
---
+++ 0x09-static_libraries/functions/0-memset.c
@@ -0,0 +1,14 @@
+#include "../main.h"
+
+/**
+ * _memset - Not implemented yet
+ * @s: This is my entry and return
+ * @b: This is the character to copy
+ * @n: This is the number of Bytes
+ *
+ * Return: This is my result and Return S
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	return (s);
+}
